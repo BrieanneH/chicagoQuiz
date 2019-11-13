a3c4992ed0e85d9befa1ec3492dace39805da24a
BrieanneH <howard.brieanne@gmail.com>
2019-11-12 23:50:11 -0600

    setting up the JavaScript structure

---
+++ questions.js
@@ -0,0 +1,33 @@
+var quizQuestions = [
+    { title: "what color is the sky?:",
+    choices:["red", "blue","green", "pink"],
+    answer: "blue" 
+    }, 
+
+    {title: "what year did Pompeii errupt?",
+    choices: ["87 A.D", "2,000 B.C", "79 A.D","57 B.C"],
+    answer: "79 A.D"
+    },
+
+    {title: "what is the capital of Washington?",
+    choices: ["Seattle", "Portland", "Olympia", "None"],
+    answer: "Olympia"
+    },
+
+]
+
+var usersChoice = "",
+
+var startQuiz = document.querySelector("#start");
+
+
+function createQuiz(){
+
+    if(choices == answers);
+
+}
+
+
+
+
+
